b5ba7f11c480b7f64bedf9af1458c7f6dfa89f73
Egi Balliu <egiballiu@gmail.com>
2024-08-24 01:13:00 +0200

    chore: update homepage animation

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,21 @@
+import Link from 'next/link'
+import Layout from '../components/layout/layout'
+
 const Custom404 = () => {
   return (
-    <div>
-      <h1>Page Not Found</h1>
-      <p>{`Sorry, we couldn find what you were looking for.`}</p>
-    </div>
+    <Layout>
+      <div className="h-[calc(100vh-200px)] w-full flex flex-col items-center pt-[10vh] gap-6 md:gap-10">
+        <h1 className="text-primary  font-secondary font-bold text-4xl md:text-6xl">
+          Oops, sei perso!😱
+        </h1>
+        <p>
+          {`Spiacenti, non siamo riusciti a trovare quello che cercavi. Torni a `}
+          <Link className="text-primary cursor-pointer" href={'/'}>
+            Home
+          </Link>
+        </p>
+      </div>
+    </Layout>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
 import Layout from '../components/layout/layout'
 import SEO from '../components/seo/seo'
+import Image from 'next/image'
+import { gsap } from 'gsap'
+import { useGSAP } from '@gsap/react'
 
-const Services = () => {
+gsap.registerPlugin(useGSAP)
+
+const Home = () => {
+  useGSAP(() => {
+    gsap.from('.logo', {
+      rotation: 360,
+      transformOrigin: '50% 50%',
+      onComplete: () => {
+        gsap.to('.logo', {
+          scale: 1.5,
+          transformOrigin: '50% 50%',
+          delay: 0.2,
+        })
+      },
+    })
+  })
+
   return (
     <>
       <SEO
@@ -10,10 +29,21 @@ const Services = () => {
       />
 
       <Layout>
-        <h2 className="text-primary">sHOME</h2>
+        <div className="h-[calc(100vh-200px)] w-full flex flex-col items-center pt-[10vh] gap-6 md:gap-10">
+          <h1 className="text-primary uppercase font-secondary font-bold text-4xl md:text-8xl">
+            Challenge
+          </h1>
+          <Image
+            className="logo"
+            src={'/favicon/favicon.png'}
+            alt="CB logo"
+            width={100}
+            height={100}
+          />
+        </div>
       </Layout>
     </>
   )
 }
 
-export default Services
+export default Home
